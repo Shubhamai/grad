cc5c84a1a060200e29b634af9326cf96b59da2ee
Shubhamai <shubham.aiengineer@gmail.com>
2024-04-11 22:42:02 +0530

    gg

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // define a function
+    fn print_number(x: i32) {
+        println!("x is: {}", x);
+    }
+
+    // define a float variable
+    let x: f64 = 1.0;
+
+    println!("Hello, world!");
+}
